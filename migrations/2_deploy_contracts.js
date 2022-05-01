08ce97f98f17826dd1ebaddc71c8103fe6a65ab9
Wayu Ragwongsiri <windywayu2000@gmail.com>
2022-05-01 16:37:33 +0700

    Add Lottery.sol

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var Adoption = artifacts.require("Adoption");
+var Adoption = artifacts.require("Lottery");
 
 module.exports = function(deployer) {
   deployer.deploy(Adoption);
+};
+
+module.exports = function(deployer) {
+  deployer.deploy(Lottery);
 };
