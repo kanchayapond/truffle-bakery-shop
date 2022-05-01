3a842eefbd8f8e50a1694133c2095f70bcf879ae
cusniwtt <37668108+cusniwtt@users.noreply.github.com>
2022-05-01 21:03:07 +0700

    remove lottery.sol

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,5 @@
 var Adoption = artifacts.require("Adoption");
-var Adoption = artifacts.require("Lottery");
 
 module.exports = function(deployer) {
   deployer.deploy(Adoption);
-};
-
-module.exports = function(deployer) {
-  deployer.deploy(Lottery);
 };
