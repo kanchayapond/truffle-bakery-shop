8a99ff747e01335aaed32ed3cf075efa1fe39227
cusniwtt <37668108+cusniwtt@users.noreply.github.com>
2022-05-02 15:20:27 +0700

    Update instalmet

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.5.0;
 
 contract Adoption {
@@ -14,4 +15,7 @@ contract Adoption {
     function getAdopters() public view returns (address[16] memory) {
         return adopters;
     }
+    
+    //function Instalment
+
 }
