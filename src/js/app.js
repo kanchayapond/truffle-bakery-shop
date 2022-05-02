ca422fe712e841a1415df2b8db142663d36e510b
cusniwtt <37668108+cusniwtt@users.noreply.github.com>
2022-05-02 17:22:56 +0700

    fix app.js

--- src/js/app.js
+++ src/js/app.js
@@ -59,12 +59,23 @@ App = {
       // Use our contract to retrieve and mark the adopted pets
       return App.markAdopted();
     });
+    $.getJSON('Installment.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with @truffle/contract
+      var InstallmentArtifact = data;
+      App.contracts.Installment = TruffleContract(InstallmentArtifact);
+    
+      // Set the provider for our contract
+      App.contracts.Installment.setProvider(App.web3Provider);
+    
+      // Use our contract to retrieve and mark the adopted pets
+      return App.markAdopted();
+    });
     return App.bindEvents();
   },
 
   bindEvents: function() {
     $(document).on('click', '.btn-adopt', App.handleAdopt);
-    $(document).on('click', '.btn-Installment', App.handleAdopt);
+    $(document).on('click', '.btn-Installment', App.handleInstallment);
   },
 
   markAdopted: function() {
@@ -110,6 +121,36 @@ App = {
       console.log(err.message);
     });
   });
+  },
+
+  handleInstallment: function(event) {
+    event.preventDefault();
+    
+    var petId = parseInt($(event.target).data('id'));
+
+    var adoptionInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+    var account = accounts[0];
+
+    App.contracts.Adoption.deployed().then(function(instance) {
+      adoptionInstance = instance;
+
+    // Execute adopt as a transaction by sending account
+    return adoptionInstance.adopt(petId, {from: account});
+    }).then(function(result) {
+      return App.markAdopted();
+    }).catch(function(err) {
+      console.log(err.message);
+    });
+  });
+
+  //window.alert("sometext");
+
   }
 };
 
